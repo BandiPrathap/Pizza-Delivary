1e0e318e40dbf3f740f82d15a76270a92851503d
PRATHAP BANDI <126871309+BandiPrathap@users.noreply.github.com>
2024-07-03 16:26:55 +0530

    make some upadates on src folder

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import WellcomePage from "./pages/WellcomePage";
+import SignUp from './pages/SignUpPage';
+import SignIn from './pages/SignInPage';
+import ExampleFetch from './component/ExampleFetch'
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+     {/* <WellcomePage />*/}
+      {/*<SignUp />*/}
+      {/*<SignIn />*/}
+      <ExampleFetch />
+    </div>
+  );
+}
+
+export default App;
